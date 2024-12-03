302ad305d96ea8e02ac4ec43185b03b930ecf8ac
dm-turing <deephan.m@turing.com>
2024-12-03 10:53:06 +0000

    Modifying contents

--- 390205/turn1_modelA_sample1.go
+++ 390205/turn1_modelA_sample1.go
@@ -2,30 +2,63 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"sync/atomic"
 )
 
+var counter uint64 = 0
+
+func incrementCounter() {
+	atomic.AddUint64(&counter, 1)
+}
+
+var value uint64 = 42
+
+func getValue() uint64 {
+	return atomic.LoadUint64(&value)
+}
+
+func setValue(newValue uint64) {
+	atomic.StoreUint64(&value, newValue)
+}
+
+func swapValue(newValue uint64) uint64 {
+	return atomic.SwapUint64(&value, newValue)
+}
+
+func compareAndSwap(expected uint64, newValue uint64) bool {
+	return atomic.CompareAndSwapUint64(&value, expected, newValue)
+}
+
+var intValue int32 = 0
+
+func swapInt(newValue int32) {
+	atomic.SwapInt32(&intValue, newValue)
+}
+
+var int64Value int64 = 100
+
+func loadInt64() int64 {
+	return atomic.LoadInt64(&int64Value)
+}
+
+func storeInt64(newValue int64) {
+	atomic.StoreInt64(&int64Value, newValue)
+}
+
 func main() {
-	var value int32 = 100
-	var wg sync.WaitGroup
-	const numGoroutines = 10
-
-	wg.Add(numGoroutines)
-	for i := 0; i < numGoroutines; i++ {
-		go func(id int) {
-			defer wg.Done()
-			for {
-				oldValue := atomic.LoadInt32(&value)
-				newValue := oldValue + 1
-				if atomic.CompareAndSwapInt32(&value, oldValue, newValue) {
-					fmt.Printf("Goroutine %d updated value to: %d\n", id, newValue)
-					break
-				}
-			}
-		}(i)
-	}
-
-	wg.Wait()
-	fmt.Printf("Final value: %d\n", value)
+	incrementCounter()
+	fmt.Println(counter)
+	getValue()
+	fmt.Println(value)
+	setValue(1000)
+	fmt.Println(value)
+	swapValue(20000)
+	fmt.Println(value)
+	compareAndSwap(atomic.LoadUint64(&value), 30000)
+	fmt.Println(value)
+	swapInt(20000)
+	fmt.Println(intValue)
+	fmt.Println(loadInt64())
+	storeInt64(30000)
+	fmt.Println(int64Value)
 }
