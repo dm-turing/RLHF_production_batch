1538b5dc94448f246497441fd9c8bfe502e286f7
dm-turing <deephan.m@turing.com>
2024-12-12 04:29:29 +0530

    Adding task files for 390689

--- Week 49/390689/main.go
+++ Week 49/390689/main.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/google/uuid"
-	"github.com/sirupsen/logrus"
+	"sync"
+	"time"
 )
 
+func doWorkWithTimeout(id int, done chan struct{}) {
+	defer func() { done <- struct{}{} }()
+	time.Sleep(time.Duration(id) * time.Second)
+	if id%2 == 0 {
+		fmt.Println("Goroutine", id, "finished successfully")
+	} else {
+		fmt.Println("Goroutine", id, "encountered an error")
+	}
+}
+
 func main() {
-	fmt.Println(uuid.Max)
-	fmt.Println(logrus.GetLevel().String())
-	fmt.Println("Hello, World!")
+	var wg sync.WaitGroup
+	done := make(chan struct{}, 5)
+	timeout := time.After(3 * time.Second)
+
+	wg.Add(5)
+	for i := 1; i <= 5; i++ {
+		go func(id int) {
+			defer wg.Done()
+			doWorkWithTimeout(id, done)
+		}(i)
+	}
+
+	select {
+	case <-timeout:
+		fmt.Println("Operation timed out")
+	case <-done:
+		// At least one goroutine completed
+	}
+
+	wg.Wait()
+	close(done)
+
+	for range done {
+		// Process completed goroutines
+	}
 }
