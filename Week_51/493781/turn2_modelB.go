a1c3f4421801a058f566e5369db54211e654a8c2
dm-turing <deephan.m@turing.com>
2024-12-26 20:04:02 +0530

    Adding turn2 for 493781

--- Week_51/493781/turn2_modelB.go
+++ Week_51/493781/turn2_modelB.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
 // Function signature for a predicate that takes a string and returns a bool
@@ -13,51 +13,50 @@ type Callback func()
 
 // Function to call the specified callbacks based on the predicate
 func callByPredicate(callbacks []Callback, predicate Predicate) {
-    for _, callback := range callbacks {
-        // Apply the predicate function to the name of the callback function (as a string)
-        name := fmt.Sprintf("%v", callback)
-        if predicate(name) {
-            callback() // Call the callback function
-        }
-    }
+	for _, callback := range callbacks {
+		// Apply the predicate function to the name of the callback function (as a string)
+		name := fmt.Sprintf("%v", callback)
+		if predicate(name) {
+			callback() // Call the callback function
+		}
+	}
 }
 
 // Named function to say "Hello"
 func sayHello() {
-    fmt.Println("Hello!")
-}
-
-// Anonymous function to say "Hi"
-hiFn := func() {
-    fmt.Println("Hi!")
+	fmt.Println("Hello!")
 }
 
 // Named function to say "Greetings"
 func sayGreetings() {
-    fmt.Println("Greetings!")
+	fmt.Println("Greetings!")
 }
 
 func main() {
-    // Create a slice of callback functions
-    callbacks := []Callback{
-        sayHello,        // Named function
-        hiFn,           // Anonymous function
-        sayGreetings,     // Named function
-    }
+	// Anonymous function to say "Hi"
+	hiFn := func() {
+		fmt.Println("Hi!")
+	}
+	// Create a slice of callback functions
+	callbacks := []Callback{
+		sayHello,     // Named function
+		hiFn,         // Anonymous function
+		sayGreetings, // Named function
+	}
 
-    // Predicate function to check if the function name contains "Hello"
-    predicateContainsHello := func(name string) bool {
-        return strings.Contains(name, "Hello")
-    }
+	// Predicate function to check if the function name contains "Hello"
+	predicateContainsHello := func(name string) bool {
+		return strings.Contains(name, "Hello")
+	}
 
-    fmt.Println("Calling functions that contain 'Hello':")
-    callByPredicate(callbacks, predicateContainsHello) // Output: Hello! Greetings!
+	fmt.Println("Calling functions that contain 'Hello':")
+	callByPredicate(callbacks, predicateContainsHello) // Output: Hello! Greetings!
 
-    // Predicate function to check if the function name is even length
-    predicateEvenLength := func(name string) bool {
-        return len(name)%2 == 0
-    }
+	// Predicate function to check if the function name is even length
+	predicateEvenLength := func(name string) bool {
+		return len(name)%2 == 0
+	}
 
-    fmt.Println("\nCalling functions with even-length names:")
-    callByPredicate(callbacks, predicateEvenLength) // Output: Hi! Greetings!
-} 
+	fmt.Println("\nCalling functions with even-length names:")
+	callByPredicate(callbacks, predicateEvenLength) // Output: Hi! Greetings!
+}
